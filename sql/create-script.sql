2f7b247da798c2763a1ab04f9df91923cf1f8124
FernandoMM2003 <fernandommarques2010@gmail.com>
2024-08-01 21:52:02 -0300

    Adicionando scripts conexão ao rds, criação do banco e insert de dados tratados

---
+++ sql/create-script.sql
@@ -0,0 +1,97 @@
+create database AntiPlague;
+
+use AntiPlague;
+
+-- Tabela principal que contém todos os campos
+CREATE TABLE covid_data (
+    location VARCHAR(255),
+    date DATE,
+    total_cases FLOAT,
+    new_cases FLOAT,
+    total_deaths FLOAT,
+    new_deaths FLOAT,
+    reproduction_rate FLOAT,
+    icu_patients FLOAT,
+    hosp_patients FLOAT,
+    total_tests FLOAT,
+    new_tests FLOAT,
+    tests_per_case FLOAT,
+    total_vaccinations FLOAT,
+    people_vaccinated FLOAT,
+    people_fully_vaccinated FLOAT,
+    population_density FLOAT,
+    median_age FLOAT,
+    aged_65_older FLOAT,
+    aged_70_older FLOAT,
+    gdp_per_capita FLOAT,
+    extreme_poverty FLOAT,
+    handwashing_facilities FLOAT,
+    life_expectancy FLOAT,
+    population FLOAT,
+    PCA1 FLOAT,
+    PCA2 FLOAT,
+    PCA3 FLOAT,
+    PCA4 FLOAT,
+    cluster INT
+);
+
+-- Tabela de informações gerais de casos e mortes
+CREATE TABLE cases_deaths (
+    location VARCHAR(255),
+    date DATE,
+    total_cases FLOAT,
+    new_cases FLOAT,
+    total_deaths FLOAT,
+    new_deaths FLOAT,
+    reproduction_rate FLOAT,
+    PRIMARY KEY (location, date)
+);
+
+-- Tabela de dados de hospitalização
+CREATE TABLE hospitalization (
+    location VARCHAR(255),
+    date DATE,
+    icu_patients FLOAT,
+    hosp_patients FLOAT,
+    PRIMARY KEY (location, date)
+);
+
+-- Tabela de testes e vacinação
+CREATE TABLE tests_vaccinations (
+    location VARCHAR(255),
+    date DATE,
+    total_tests FLOAT,
+    new_tests FLOAT,
+    tests_per_case FLOAT,
+    total_vaccinations FLOAT,
+    people_vaccinated FLOAT,
+    people_fully_vaccinated FLOAT,
+    PRIMARY KEY (location, date)
+);
+
+-- Tabela de dados demográficos e socioeconômicos
+CREATE TABLE demographics_economics (
+    location VARCHAR(255),
+    population_density FLOAT,
+    median_age FLOAT,
+    aged_65_older FLOAT,
+    aged_70_older FLOAT,
+    gdp_per_capita FLOAT,
+    extreme_poverty FLOAT,
+    handwashing_facilities FLOAT,
+    life_expectancy FLOAT,
+    population FLOAT,
+    PRIMARY KEY (location)
+);
+
+-- Tabela de dados de PCA e cluster
+CREATE TABLE pca_cluster (
+    location VARCHAR(255),
+    date DATE,
+    PCA1 FLOAT,
+    PCA2 FLOAT,
+    PCA3 FLOAT,
+    PCA4 FLOAT,
+    cluster INT,
+    PRIMARY KEY (location, date)
+);
