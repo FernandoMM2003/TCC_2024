fe4c39b64a15dd6bbcaaf48cbdaddc2c7bc3a10f
FernandoMM2003 <fernandommarques2010@gmail.com>
2024-08-12 23:37:07 -0300

    Ajustes

--- sql/create-script.sql
+++ sql/create-script.sql
@@ -2,6 +2,9 @@ create database AntiPlague;
 
 use AntiPlague;
 
+select count(*) from covid_data;
+select count(*) from covid_data_total;
+
 -- Tabela principal que contém todos os campos
 CREATE TABLE covid_data (
     location VARCHAR(255),
@@ -34,6 +37,40 @@ CREATE TABLE covid_data (
     PCA4 FLOAT,
     cluster INT
 );
+
+-- Tabela principal que contém todos os campos
+CREATE TABLE covid_data_total (
+    location VARCHAR(255),
+    date DATE,
+    total_cases FLOAT,
+    new_cases FLOAT,
+    total_deaths FLOAT,
+    new_deaths FLOAT,
+    reproduction_rate FLOAT,
+    icu_patients FLOAT,
+    hosp_patients FLOAT,
+    total_tests FLOAT,
+    new_tests FLOAT,
+    tests_per_case FLOAT,
+    total_vaccinations FLOAT,
+    people_vaccinated FLOAT,
+    people_fully_vaccinated FLOAT,
+    population_density FLOAT,
+    median_age FLOAT,
+    aged_65_older FLOAT,
+    aged_70_older FLOAT,
+    gdp_per_capita FLOAT,
+    extreme_poverty FLOAT,
+    handwashing_facilities FLOAT,
+    life_expectancy FLOAT,
+    population FLOAT,
+    PCA1 FLOAT,
+    PCA2 FLOAT,
+    PCA3 FLOAT,
+    PCA4 FLOAT,
+    cluster INT
+);
+
 
 -- Tabela de informações gerais de casos e mortes
 CREATE TABLE cases_deaths (
